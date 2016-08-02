2a81699e3ec75f8bd62e4d839db02eda80d22895
litao <litao@sogou-inc.com>
2016-08-02 23:08:23 +0800

    Support dynamicly update hadop config file.

--- bin/format_namenode.sh
+++ bin/format_namenode.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+tmpDir=/tmp/hadoop-root
+
+if [ $# -eq 1 ]; then
+  tmpDir=$1
+fi
+
 cd /search/hadoop
+sed -i "s/\$hadoop.tmp.dir/$tmpDir/g" etc/hadoop/core-site.xml
+
 bin/hdfs namenode -format
