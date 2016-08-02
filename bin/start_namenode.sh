d3ea311716f00dda4b7c0c94e42bd345b7f177b0
litao <litao@sogou-inc.com>
2016-08-02 23:07:05 +0800

    Support dynamicly update hadop config file.

--- bin/start_namenode.sh
+++ bin/start_namenode.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+tmpDir=/tmp/hadoop-root
+
+if [ $# -eq 1 ]; then
+  tmpDir=$1
+fi
+
 cd /search/hadoop
+sed -i "s/\$hadoop.tmp.dir/$tmpDir/g" etc/hadoop/core-site.xml
+
 sbin/hadoop-daemon.sh --config etc/hadoop --script hdfs start namenode
 while true; do sleep 60; done
